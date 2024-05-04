dfc40c9d9f92b017fbf863c5be9885d83c26da9e
Samuel Cedeno <samuelcd311@outlook.es>
2024-05-04 11:52:29 -0400

    about page

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
     path ("",HomePageView.as_view(), name="home")
+    path ("about/",AboutPageView.as_view(), name="about")
 ]
 #Los corchetes son usados para hacer listas
 #La segunda coma es el visualizador de la pagina (lo que se va a mostrar)
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import TemplateView
 
 # Create your views here.
-class HomePageView(TemplateView):
-#Solo los nombres de las clases se escriben incialmente con mayusculas
+class HomePageView (TemplateView):
+#Solo los nombres de las clases se escriben incialmente con mayusculas y al final los dos puntos :
     template_name = "home.html"
+
+class AboutPageView (TemplateView):
+    template_name = "about.html"
